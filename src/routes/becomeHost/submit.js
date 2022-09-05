17f894614ea1cbd450972fd2ba11f544dce61c64
RajuM1997 <mraju5797@gmail.com>
2022-09-06 01:34:34 +0600

    error message fiexd

--- src/routes/becomeHost/submit.js
+++ src/routes/becomeHost/submit.js
@@ -1,15 +1,15 @@
 // Fetch Request
-import fetch from '../../core/fetch';
+import fetch from "../../core/fetch";
 
 // Toaster
 import { toastr } from "react-redux-toastr";
 
 const submit = async (values) => {
-    console.log("values", values);
-    if (values == null) {
-        toastr.error("Error Occurred", "Failed to add new listing");
-    } else {
-        const mutation = `
+  console.log("values", values);
+  if (values == null) {
+    toastr.error("Error Occurred", "Failed to add new listing");
+  } else {
+    const mutation = `
           mutation createHostListing(
             $userId: Int,
             $categoryId: Int,
@@ -79,27 +79,33 @@ const submit = async (values) => {
           }
         `;
 
-        const resp = await fetch("/graphql", {
-            method: "post",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                query: mutation,
-                variables: {
-                    ...values,
-                    dynamicFields: JSON.stringify(values?.dynamicFields),
-                    coverPhoto: JSON.stringify(values?.coverPhoto),
-                    basePrice: Number(values?.basePrice),
-                },
-            }),
-            credentials: "include",
-        });
+    const resp = await fetch("/graphql", {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: mutation,
+        variables: {
+          ...values,
+          dynamicFields: JSON.stringify(values?.dynamicFields),
+          coverPhoto: JSON.stringify(values?.coverPhoto),
+          basePrice: Number(values?.basePrice),
+        },
+      }),
+      credentials: "include",
+    });
 
-        const { data } = await resp.json();
-        console.log(data);
+    const { data } = await resp.json();
+    if (data?.createHostListing.status === "success") {
+      toastr.success("Listing", "Successfully Added!");
+      // history.push("/siteadmin/category");
+    } else {
+      toastr.error("Listing", "Failed to Added");
     }
+    console.log(data);
+  }
 };
 
 export default submit;
